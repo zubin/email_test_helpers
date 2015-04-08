fc07a9391663a8ce0bfc3bf197b8c24e990bf25b
Zubin Henner <zubin@rubidium.com.au>
2015-04-09 03:18:34 +1000

    Suppress deprecation warning in test

--- features/step_definitions/email_steps.rb
+++ features/step_definitions/email_steps.rb
@@ -5,6 +5,11 @@ class Mailer < ActionMailer::Base
   end
 end
 
+def deliver_method
+  supports_deliver_now = ActionMailer.version >= Gem::Version.new('4.2.0')
+  supports_deliver_now ? :deliver_now : :deliver
+end
+
 Given(/^a "(.*?)" email sent to "(.*?)" containing:$/) do |subject, recipient, body|
-  Mailer.notify(to: recipient, subject: subject, body: body).deliver
+  Mailer.notify(to: recipient, subject: subject, body: body).send deliver_method
 end
